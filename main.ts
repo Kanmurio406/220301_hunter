5f0c1a1c251dc081de16ebf25fedfb5f0c109f51
Kuan-Yu Chou <jenny2311@gmail.com>
2022-03-01 11:36:48 +0800

    加減猜測數字、廣播功能

--- main.ts
+++ main.ts
@@ -1,3 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    target += -1
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(target)
+    radio.sendValue("id", 3)
+    radio.sendValue("try", target)
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    target += 1
+})
+let target = 0
+radio.setGroup(99)
+basic.showIcon(IconNames.Heart)
+target = 50
 basic.forever(function () {
 	
 })
